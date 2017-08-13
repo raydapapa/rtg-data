8ddf721892d9634c5ff8c3cee105e2e8a55b272c
Papa Ray <disciple.ray@icloud.com>
2017-08-14 00:41:05 +0800

    Created project

---
+++ src/main/java/club/realtg/rtgdata/controllers/HelloController.java
@@ -0,0 +1,23 @@
+package club.realtg.rtgdata.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Desc
+ *
+ * @author Papa Ray
+ * Created on 2017-08-13
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Hello World!";
+    }
+
+
+}
