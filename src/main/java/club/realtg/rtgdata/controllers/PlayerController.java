caea563df277fd29e4a69f9999f9bc3ac5890270
Papa Ray <disciple.ray@icloud.com>
2017-08-14 02:51:02 +0800

    added some code

--- src/main/java/club/realtg/rtgdata/controllers/PlayerController.java
+++ src/main/java/club/realtg/rtgdata/controllers/PlayerController.java
@@ -1,8 +1,14 @@
 package club.realtg.rtgdata.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 /**
  * Desc
@@ -11,13 +17,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created on 2017-08-13
  */
 @Controller
+@RequestMapping("player")
 public class PlayerController {
 
+    static ArrayList<String> listPlayerNames = new ArrayList<>();
+
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Hello World!";
+    public String gotoPlayerList(Model model) {
+        String title = "球员列表";
+
+//        listPlayerNames.add("邵宇驰");
+//        listPlayerNames.add("李万郁");
+//        listPlayerNames.add("张超");
+//        listPlayerNames.add("秦笑");
+//        listPlayerNames.add("罗渝力");
+//        listPlayerNames.add("吴林青");
+//        listPlayerNames.add("曾理");
+//        listPlayerNames.add("向军");
+//        listPlayerNames.add("李双");
+//        listPlayerNames.add("刘浩");
+
+        model.addAttribute("title",title);
+        model.addAttribute("playerNames",listPlayerNames);
+        return "player/playerList";
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addNewPlayer(Model model) {
+        String title = "新增球员";
+        model.addAttribute("title",title);
+        return "player/addPlayer";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddPlayerForm(@RequestParam String playerName) {
+        listPlayerNames.add(playerName);
+        // Redirect to /player
+        return "redirect:";
+    }
 
 }
