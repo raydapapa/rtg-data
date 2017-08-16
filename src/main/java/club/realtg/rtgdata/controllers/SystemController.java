afe22be54c73c369ed9ee3f08a183fdf9a87f313
Papa Ray <disciple.ray@icloud.com>
2017-08-16 17:46:11 +0800

    更换bootstrap版本到v3.3.7 加入index页面

---
+++ src/main/java/club/realtg/rtgdata/controllers/SystemController.java
@@ -0,0 +1,20 @@
+package club.realtg.rtgdata.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller for System
+ *
+ * @author Papa Ray
+ * Created on 2017-08-16
+ */
+@Controller
+public class SystemController {
+
+    @RequestMapping(value = "")
+    public String index() {
+        return "index";
+    }
+
+}
