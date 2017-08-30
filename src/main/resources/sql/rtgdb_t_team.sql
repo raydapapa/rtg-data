63589b9991bc8cebbb8cc76d6265bf2cb693ff9e
Papa Ray <disciple.ray@icloud.com>
2017-08-30 17:52:43 +0800

    录入上赛季球队数据

---
+++ src/main/resources/sql/rtgdb_t_team.sql
@@ -0,0 +1,11 @@
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:25:11', null, '2017-08-30 14:25:11', '清一色');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:25:22', null, '2017-08-30 14:25:22', '华润Sock');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:25:46', null, '2017-08-30 14:25:46', '银师联');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:25:58', null, '2017-08-30 14:25:58', '一中88级');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:15', null, '2017-08-30 14:26:15', '大九联盟');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:24', null, '2017-08-30 14:26:24', '潼南FC');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:31', null, '2017-08-30 14:26:31', '朋友圈');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:41', null, '2017-08-30 14:26:41', '让胖娃飞');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:47', null, '2017-08-30 14:26:47', '快乐足球');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:52', null, '2017-08-30 14:26:52', '火狼');
+INSERT INTO rtgdb.t_team (ctime, description, mtime, name) VALUES ('2017-08-30 14:26:59', null, '2017-08-30 14:26:59', '黑莓FC');
